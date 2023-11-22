99fda06a1aa0ef4aaccada4b3f31df614f5ee508
Harry Yang <i.take.today@gmail.com>
2023-11-22 22:47:59 +0800

    :sparkles: ServiceProviderChannelConnector for tcp service provider

---
+++ today-service-provider/src/main/java/cn/taketoday/cloud/provider/ServiceProviderChannelConnector.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2021 - 2023 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.cloud.provider;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Objects;
+
+import cn.taketoday.cloud.RpcRequest;
+import cn.taketoday.cloud.RpcResponse;
+import cn.taketoday.cloud.core.serialize.DeserializeFailedException;
+import cn.taketoday.cloud.core.serialize.JdkSerialization;
+import cn.taketoday.cloud.core.serialize.Serialization;
+import cn.taketoday.cloud.netty.ChannelConnector;
+import cn.taketoday.cloud.registry.ServiceNotFoundException;
+import cn.taketoday.lang.Nullable;
+import cn.taketoday.reflect.MethodInvoker;
+import cn.taketoday.util.ClassUtils;
+import cn.taketoday.util.MapCache;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 1.0 2023/11/22 22:35
+ */
+public class ServiceProviderChannelConnector extends ChannelConnector {
+
+  private final LocalServiceHolder serviceHolder;
+
+  /** for serialize and deserialize */
+  private final Serialization<RpcRequest> serialization;
+
+  /** fast method mapping cache */
+  private final MethodMapCache methodMapCache = new MethodMapCache();
+
+  public ServiceProviderChannelConnector(LocalServiceHolder serviceHolder) {
+    this(serviceHolder, new JdkSerialization<>());
+  }
+
+  public ServiceProviderChannelConnector(LocalServiceHolder serviceHolder, Serialization<RpcRequest> serialization) {
+    this.serialization = serialization;
+    this.serviceHolder = serviceHolder;
+  }
+
+  @Override
+  protected void initChannel(Channel ch, ChannelPipeline pipeline) throws Exception {
+    pipeline.addLast("tcp-frame-decoder", new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE,
+            0, 4, 0, 4));
+  }
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    if (msg instanceof ByteBuf byteBuf) {
+      RpcResponse response = handle(ctx, byteBuf);
+      ByteBuf buffer = ctx.alloc().ioBuffer(128);
+      serialization.serialize(response, new ByteBufOutputStream(buffer));
+      ctx.writeAndFlush(buffer);
+    }
+    else {
+      ctx.fireChannelRead(msg);
+    }
+  }
+
+  protected RpcResponse handle(ChannelHandlerContext ctx, ByteBuf byteBuf) throws IOException {
+    try (ByteBufInputStream inputStream = new ByteBufInputStream(byteBuf)) {
+      try {
+        RpcRequest request = serialization.deserialize(inputStream);
+        Object service = serviceHolder.getService(request.getServiceName());
+        if (service == null) {
+          return RpcResponse.ofThrowable(new ServiceNotFoundException(request.getServiceName()));
+        }
+        MethodInvoker invoker = methodMapCache.get(new MethodCacheKey(request), service);
+        Object[] args = request.getArguments();
+        return createResponse(service, args, invoker);
+      }
+      catch (IOException io) {
+        return RpcResponse.ofThrowable(new DeserializeFailedException(io));
+      }
+      catch (ClassNotFoundException e) {
+        return RpcResponse.ofThrowable(new ServiceNotFoundException(e));
+      }
+      catch (Throwable e) {
+        return RpcResponse.ofThrowable(e);
+      }
+    }
+  }
+
+  private RpcResponse createResponse(Object service, Object[] args, MethodInvoker invoker) {
+    if (invoker == null) {
+      return RpcResponse.ofThrowable(new ServiceNotFoundException());
+    }
+    try {
+      return new RpcResponse(invoker.invoke(service, args));
+    }
+    catch (Throwable e) {
+      return RpcResponse.ofThrowable(e);
+    }
+  }
+
+  private static final class MethodMapCache extends MapCache<MethodCacheKey, MethodInvoker, Object> {
+
+    @Override
+    protected MethodInvoker createValue(MethodCacheKey key, @Nullable Object service) {
+      Method methodToUse = getMethod(key, service);
+      if (methodToUse == null) {
+        return null;
+      }
+      return MethodInvoker.fromMethod(methodToUse);
+    }
+
+    private static Method getMethod(MethodCacheKey key, Object service) {
+      String method = key.method;
+      String[] paramTypes = key.paramTypes;
+      int parameterLength = paramTypes.length;
+
+      Class<Object> serviceImpl = ClassUtils.getUserClass(service);
+      for (Method serviceMethod : serviceImpl.getMethods()) {
+        if (Objects.equals(serviceMethod.getName(), method)
+                && parameterLength == serviceMethod.getParameterCount()) {
+          int current = 0;
+          boolean equals = true;
+          for (Class<?> parameterType : serviceMethod.getParameterTypes()) {
+            if (!parameterType.getName().equals(paramTypes[current++])) {
+              // not target method
+              equals = false;
+              break;
+            }
+          }
+          if (equals) {
+            return serviceMethod;
+          }
+        }
+      }
+      return null;
+    }
+  }
+
+  private static class MethodCacheKey {
+    public final String method;
+    public final String[] paramTypes;
+
+    MethodCacheKey(RpcRequest request) {
+      this.method = request.getMethod();
+      this.paramTypes = request.getParamTypes();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o)
+        return true;
+      if (!(o instanceof MethodCacheKey that))
+        return false;
+      return Objects.equals(method, that.method)
+              && Arrays.equals(paramTypes, that.paramTypes);
+    }
+
+    @Override
+    public int hashCode() {
+      return 31 * Objects.hash(method) + Arrays.hashCode(paramTypes);
+    }
+  }
+}
